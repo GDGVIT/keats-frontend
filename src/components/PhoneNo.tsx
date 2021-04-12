b7d38d871be558bc211b783f6cb7795aceeb67be
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-04-12 16:09:37 +0530

    feat: added phone country code

--- src/components/PhoneNo.tsx
+++ src/components/PhoneNo.tsx
@@ -1,16 +1,22 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { AppContext } from './../Context'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import './../styles/Form.css'
+import './../styles/PhoneNo.css'
+// import PhoneInput from 'react-phone-number-input'
+import PhoneInput from 'react-phone-number-input'
+import 'react-phone-number-input/style.css'
 
-const PhoneNo : React.FC= () => {
+const PhoneNo: React.FC = () => {
   const { stageState } = useContext(AppContext)
   const [, setStage] = stageState
 
+  const [value, setValue] = useState('')
+
   useEffect(() => {
     (window as any).recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
-      size: 'invisible',
+      size: 'invisible'
       // callback: (response: any) => {
       //   // reCAPTCHA solved, allow signInWithPhoneNumber.
       // Redirect to clubs
@@ -21,7 +27,8 @@ const PhoneNo : React.FC= () => {
   const handleSubmit = async (e: React.BaseSyntheticEvent): Promise<any> => {
     try {
       e.preventDefault()
-      const phoneNumber: string = e.target[0].value
+      // const phoneNumber: string = e.target[0].value
+      const phoneNumber: string = value
       const appVerifier = (window as any).recaptchaVerifier
       const confirmationResult = await firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier);
       (window as any).confirmationResult = confirmationResult
@@ -34,7 +41,13 @@ const PhoneNo : React.FC= () => {
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        <input name='phone' type='tel' placeholder='Enter your Phone Number' required />
+        {/* <input name='phone' type='tel' placeholder='Enter your Phone Number' required /> */}
+        <PhoneInput
+          international
+          defaultCountry='IN'
+          value={value}
+          onChange={setValue}
+        />
       </label>
       <button type='submit' id='sign-in-button'>Get OTP</button>
     </form>
